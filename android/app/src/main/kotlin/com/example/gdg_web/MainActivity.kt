9de8b3cfd6f751eb76bfb72c72f9817567dbb43e
Salihcan Kahya <salihcankahya@gmail.com>
2020-12-20 19:42:26 +0300

    Project init

---
+++ android/app/src/main/kotlin/com/example/gdg_web/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gdg_web
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'GDG Kocaeli',
+      home: HomePageView(),
+    );
+  }
+}
+
+class HomePageView extends StatefulWidget {
+  @override
+  _HomePageViewState createState() => _HomePageViewState();
+}
+
+class _HomePageViewState extends State<HomePageView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Selam GDG Kocaeli"),
+      ),
+    );
+  }
+}
